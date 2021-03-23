177d1581d397ffa77662dae7b298055a170f327f
James Galantino <james_galantino@aol.com>
2021-03-22 20:33:05 -0400

    Redefined api routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 export default {
-  getBooks: function () {
-    return axios.get('/api/books');
+  searchBooks: function (userInput) {
+    return axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${userInput}`
+    );
   },
-  getBook: function (id) {
-    return axios.get('/api/books/' + id);
+  getSavedBooks: function () {
+    return axios.get('/api/books');
   },
   deleteBook: function (id) {
     return axios.delete('/api/book/' + id);
