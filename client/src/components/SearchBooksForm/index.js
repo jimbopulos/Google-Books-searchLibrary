746205438b9f373d81d7a9bbb5db334dbfadbfbb
James Galantino <james_galantino@aol.com>
2021-03-22 22:25:39 -0400

    Added submit form func for searchbooksform component

--- client/src/components/SearchBooksForm/index.js
+++ client/src/components/SearchBooksForm/index.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import { useStoreContext } from '../../utils/GlobalState';
+import React, { useState, useEffect } from 'react';
+// import { useStoreContext } from '../../utils/GlobalState';
 import API from '../../utils/API';
 
 function SearchBooksForm() {
-  const [state, dispatch] = useStoreContext();
+  const [books, setBooks] = useState([]);
+  const [booksSearch, setBooksSearch] = useState('');
 
-  const handleInputChange = async () => {};
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  const handleInputChange = async (e) => {
+    const { value } = e.target;
+
+    setBooksSearch(value);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      const { data } = await API.searchBooks(booksSearch);
+      setBooks(data);
+      console.log();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getBooks = async () => {
     const { data } = await API.searchBooks();
 
-    console.log(data);
+    // console.log(data);
+    setBooks(data);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type='text' />
-        <button type='submit'>Search</button>
+      <form>
+        <input type='text' onChange={handleInputChange} />
+        <button type='submit' onClick={handleSubmit}>
+          Search
+        </button>
       </form>
     </div>
   );
