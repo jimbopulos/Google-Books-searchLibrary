6ead3c386457cb5e34ecba6a36f4fb6d932be84f
James Galantino <james_galantino@aol.com>
2021-03-24 01:02:27 -0400

    Styled navbar

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
+import './style.css';
 
 function Jumbotron({ children }) {
-  return (
-    <div
-      style={{
-        height: 200,
-        clear: 'both',
-        paddingTop: 80,
-        textAlign: 'center',
-      }}
-      className='jumbotron'
-    >
-      {children}
-    </div>
-  );
+  return <div className='jumbotron'>{children}</div>;
 }
 
 export default Jumbotron;
